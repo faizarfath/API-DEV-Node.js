57b86a8148a6da161cd6f688024e0296a116cb25
Faiz Arfath S <133751507+faizarfath@users.noreply.github.com>
2024-06-05 11:48:45 +0530

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,8 @@
-const bcrypt = require('bcrypt');
-const connection = require('../utils/mysqlConnection');
+const loginModel = require('../models/loginModel');
 const { comparePasswords } = require('../utils/bcryptUtil');
 
 // Controller method for handling user login
-exports.loginUser = (req, res) => {
+exports.loginUser = async (req, res) => {
   const { username, password } = req.body;
 
   // Validate the input data
@@ -11,23 +10,16 @@ exports.loginUser = (req, res) => {
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
-  // Query the database to retrieve the user's hashed password
-  const query = 'SELECT * FROM LOGIN WHERE username = ?';
-  const values = [username];
-
-  connection.query(query, values, async (err, results) => {
-    if (err) {
-      console.error('Error executing query: ', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    // Retrieve the user from the database by username
+    const user = await loginModel.getUserByUsername(username);
 
     // If no user found with the provided username
-    if (results.length === 0) {
+    if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
     // Compare the provided password with the stored hashed password
-    const user = results[0];
     const passwordMatch = await comparePasswords(password, user.password);
 
     if (!passwordMatch) {
@@ -36,5 +28,27 @@ exports.loginUser = (req, res) => {
 
     // Password is correct, user is authenticated
     res.status(200).json({ message: 'Login successful', userId: user.id });
-  });
+  } catch (error) {
+    console.error('Error executing query: ', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Controller method for retrieving all rows from the LOGIN table
+exports.getAllUsers = async (req, res) => {
+  try {
+    // Retrieve all users from the database
+    const users = await loginModel.getAllUsers();
+
+    // If there are no results, return an empty array
+    if (users.length === 0) {
+      return res.status(404).json({ error: 'No users found' });
+    }
+
+    // Return the results
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error('Error executing query: ', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
